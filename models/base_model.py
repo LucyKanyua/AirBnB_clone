ad344cca88da689bba5c0b86748a212b7ab126bd
Xaldovah <dxaldovah@gmail.com>
2023-08-08 09:51:08 +0000

    class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from datetime import datetime
-import models
 import uuid
 
 
@@ -14,21 +13,19 @@ class BaseModel:
 		if kwargs:
 			for key, value in kwargs.items():
 				if key == "created_at" or key == "updated_at":
-				value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+					value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 				if key != "__class__":
-				setattr(self, key, value)
+					setattr(self, key, value)
 		else:
 			self.id = str(uuid.uuid4())
 			self.created_at = datetime.now()
 			self.updated_at = datetime.now()
-			models.storage.new(self)
 
 	def __str__(self):
 		return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
 	def save(self):
 		self.updated_at = datetime.now()
-		models.storage.save()
 
 	def to_dict(self):
 		new_dict = self.__dict__.copy()
