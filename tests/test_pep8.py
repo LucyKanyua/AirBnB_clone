da4f641421225af62cd9defe274142bf4815b5d8
Dennis Kimani <dxaldovah@gmail.com>
2023-08-12 03:42:41 -0700

    fixed the pep8 test file __init__ check with the right path

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -61,7 +61,7 @@ class Testpep8(unittest.TestCase):
             AssertionError: If the module does not follow PEP 8 coding style.
         """
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['__init__.py'])
+        p = style.check_files(['models/__init__.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_pep8_user(self):
